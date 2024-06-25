51578c8224767f80a1971958acfba7dae44cfb77
Themuuln <zerone.offical@gmail.com>
2024-06-25 21:47:03 +0800

    data: mock

---
+++ src/data/home.ts
@@ -0,0 +1,19 @@
+export default {
+  features: [
+    {
+      img: '/features/multiple-styles.png',
+      title: 'Multiple Styles',
+      description: "From classic to modern designs, we offer a wide range of styles to suit every individual's taste and preference.",
+    },
+    {
+      img: '/features/water-resistance.png',
+      title: 'Water Resistance',
+      description: 'Our watches are designed to withstand water exposure, making them perfect for everyday wear and outdoor activities.',
+    },
+    {
+      img: '/features/warranty-included.png',
+      title: 'Warranty Included',
+      description: 'We stand behind the quality of our watches with a warranty, ensuring customer satisfaction and peace of mind.',
+    },
+  ],
+};
