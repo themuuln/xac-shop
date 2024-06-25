277da05951b939974e446941279cf6f7439dc9f8
Themuuln <zerone.offical@gmail.com>
2024-06-25 21:51:05 +0800

    ui: image hover transition added

--- src/components/features/card.tsx
+++ src/components/features/card.tsx
@@ -9,8 +9,8 @@ type FeatureCardProps = {
 const FeatureCard = ({ img, title, description }: FeatureCardProps) => {
   return (
     <div className='flex flex-col space-y-6 items-center'>
-      <div className='w-full h-[260px] justify-center flex'>
-        <Image src={img} alt={title} className='object-contain' width={385} height={100} />
+      <div className='w-full overflow-hidden h-[260px] justify-center flex '>
+        <Image src={img} alt={title} className='object-contain hover:scale-110 transition-transform duration-300' width={385} height={100} />
       </div>
       <h2>{title}</h2>
       <h6 className='text-center'>{description}</h6>
