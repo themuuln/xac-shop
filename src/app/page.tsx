3549845ad312ce2c91ab0463a3c4fcd2be458a10
Themuuln <zerone.offical@gmail.com>
2024-06-25 21:57:50 +0800

    update: Readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import data from '../data/home';
 export default function Home() {
   return (
     <Container>
+      {/* Landing */}
       <section className='min-h-screen py-20 flex'>
         <div className='bg-white items-center justify-between flex rounded-3xl min-w-full'>
           <div className='w-full flex justify-center'>
@@ -23,6 +24,8 @@ export default function Home() {
           <Image src='/watches/datejust-aqua.png' alt='watch' className='object-contain' width={400} height={538.46} />
         </div>
       </section>
+
+      {/* Features */}
       <section className='min-h-screen flex items-center py-20'>
         <div className='flex flex-col space-y-8 items-center'>
           <div className='flex justify-between flex-row'>
